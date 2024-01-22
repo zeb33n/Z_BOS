42140fd2f7ad83ccd6b858e12ad9c31da8d8c288
zeb33n <zeb.bond@33n.co.uk>
2024-01-22 13:48:50 +0000

    AlPhAbEt

---
+++ zeb_alphabet.asm
@@ -0,0 +1,23 @@
+
+mov ah, 0x0e ; move to the first half of ax say its a character
+mov cl, 'A'
+mov bh, 0x0e
+mov bl, 'b'
+
+nacho_loop:
+    cmp cl, 'Z' + 1
+    je exit
+    mov al, cl
+    int 0x10
+    mov al, bl
+    int 0x10
+    add cl, 2
+    add bl, 2
+    jmp nacho_loop
+
+exit:
+    jmp $ ;)
+times 510-($-$$) db 1
+db 0x55, 0xaa
+;qemu-system-x86_64 -drive format=raw,file=boot.bin  boot.bin -nographic
+
