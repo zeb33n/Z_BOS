3f4a751db081b93e35a716563fa185d656c7c3ed
zeb33n <zeb.bond@33n.co.uk>
2024-12-19 18:16:32 +0000

    added function to write anywhere on the screen

--- src/bootloader/kernel.cpp
+++ src/bootloader/kernel.cpp
@@ -1,6 +1,14 @@
-extern "C" void main(){
-    *(char*)0xb8000 + 2 = 'Q';
-    return;
+
+void write_char (unsigned char c, unsigned char fcolour, unsigned char bcolour, int x, int y) {
+    short colours = (bcolour << 4) | (fcolour & 0x0f); //background in first 4 bits foregrounf in last 4 bits
+    volatile short *where; 
+    where = (volatile short*)0xb8000 + (y * 80 + x); 
+    *where = c | (colours << 8); // load the value into the pointer
+
 }
 
-void write_char (unsigned char c, unsigned char fcolour, unsigned char bcolour)
+extern "C" int main() {
+    write_char('A', 0xA, 0x1, 30, 2);
+    return 0;
+}
+
