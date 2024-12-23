e581ff231f4a806e9ec4acfe9c8b9d6ab320f770
zeb33n <zeb.bond@33n.co.uk>
2024-12-23 18:37:44 +0000

    working interrupts

--- src/kernel/idt.cpp
+++ src/kernel/idt.cpp
@@ -2,39 +2,7 @@
 
 extern "C" void _idt_load();
 
-extern "C" void _isr0();
-extern "C" void _isr1();
-extern "C" void _isr2();
-extern "C" void _isr3();
-extern "C" void _isr4();
-extern "C" void _isr5();
-extern "C" void _isr6();
-extern "C" void _isr7();
-extern "C" void _isr8();
-extern "C" void _isr9();
-extern "C" void _isr10();
-extern "C" void _isr11();
-extern "C" void _isr12();
-extern "C" void _isr13();
-extern "C" void _isr14();
-extern "C" void _isr15();
-extern "C" void _isr16();
-extern "C" void _isr17();
-extern "C" void _isr18();
-extern "C" void _isr19();
-extern "C" void _isr20();
-extern "C" void _isr21();
-extern "C" void _isr22();
-extern "C" void _isr23();
-extern "C" void _isr24();
-extern "C" void _isr25();
-extern "C" void _isr26();
-extern "C" void _isr27();
-extern "C" void _isr28();
-extern "C" void _isr29();
-extern "C" void _isr30();
-extern "C" void _isr31();
-extern "C" void _syscall();
+extern "C" void _isr_generic();
 
 // todo move to memory file
 void* memset(void* dest, unsigned char val, int count) {
@@ -55,30 +23,14 @@ struct idt_entry {
 
 struct idt_ptr {
   unsigned short limit;
-  unsigned long base;
+  unsigned int base;
 } __attribute__((packed));
 
 struct idt_entry idt[256];
 struct idt_ptr _idtp;
 
-typedef struct regs {
-  unsigned int gs, fs, es, ds;                          // pushed the segs last
-  unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;  // pushed by 'pusha'
-  unsigned int int_no, err_code;  // our 'push byte #' and ecodes do this
-  unsigned int eip, cs, eflags, useresp,
-      ss;  // pushed by the processor automatically
-} __attribute__((packed)) regs;
-
-extern "C" void _fault_handler(struct regs* r) {
-  // write_base_int(r->int_no, 10, 0xE, 0x1, 1, 1);
-  // write_char(r->int_no, 0xE, 0x1, 1, 1);
-  if (r->int_no == -0x80) {  // heh, unsigned, lazy
-    write_char('e', 0xE, 0x1, 20, 20);
-  }
-
-  if (r->int_no < 32) {
-    write_char('e', 0xE, 0x1, 20, 20);
-  }
+extern "C" void _fault_handler() {
+  write_char('e', 0xE, 0x1, 20, 20);
 }
 
 void idt_set_gate(unsigned char num,
@@ -101,11 +53,6 @@ void idt_install() {
 
   // Clear out the entire IDT, initializing it to zeros
   write_base_int((unsigned long)&idt, 16, 0xF, 0x1, 1, 1);
-  write_base_int((unsigned long)_isr0, 16, 0xF, 0x1, 1, 2);
-  write_base_int((unsigned long)_isr31, 16, 0xF, 0x1, 1, 3);
-  write_base_int((unsigned long)_isr2, 16, 0xF, 0x1, 1, 4);
-  // write_base_int((unsigned long)_isr3, 16, 0xF, 0x1, 1, 5);
-  // write_base_int((unsigned long)_isr4, 16, 0xF, 0x1, 1, 6);
   memset(&idt, 0, sizeof(struct idt_entry) * 256);
 
   _idt_load();
@@ -113,37 +60,5 @@ void idt_install() {
 
 // theres nos issues with the code. just where it is. its overwriting itself?!
 void isr_install() {
-  // idt_set_gate(0, (unsigned long)_isr0, 0x08, 0x8E);
-  // idt_set_gate(1, (unsigned long)_isr1, 0x08, 0x8E);
-  // idt_set_gate(2, (unsigned long)_isr2, 0x08, 0x8E);
-  // idt_set_gate(3, (unsigned long)_isr3, 0x08, 0x8E);
-  // idt_set_gate(4, (unsigned long)_isr4, 0x08, 0x8E);
-  // idt_set_gate(5, (unsigned long)_isr5, 0x08, 0x8E);
-  // idt_set_gate(6, (unsigned long)_isr6, 0x08, 0x8E);
-  // idt_set_gate(7, (unsigned long)_isr7, 0x08, 0x8E);
-  // idt_set_gate(8, (unsigned long)_isr8, 0x08, 0x8E);
-  // idt_set_gate(9, (unsigned long)_isr9, 0x08, 0x8E);
-  // idt_set_gate(10, (unsigned long)_isr10, 0x08, 0x8E);
-  // idt_set_gate(11, (unsigned long)_isr11, 0x08, 0x8E);
-  // idt_set_gate(12, (unsigned long)_isr12, 0x08, 0x8E);
-  // idt_set_gate(13, (unsigned long)_isr13, 0x08, 0x8E);
-  // idt_set_gate(14, (unsigned long)_isr14, 0x08, 0x8E);
-  // idt_set_gate(15, (unsigned long)_isr15, 0x08, 0x8E);
-  // idt_set_gate(16, (unsigned long)_isr16, 0x08, 0x8E);
-  // idt_set_gate(17, (unsigned long)_isr17, 0x08, 0x8E);
-  // idt_set_gate(18, (unsigned long)_isr18, 0x08, 0x8E);
-  // idt_set_gate(19, (unsigned long)_isr19, 0x08, 0x8E);
-  // idt_set_gate(20, (unsigned long)_isr20, 0x08, 0x8E);
-  // idt_set_gate(21, (unsigned long)_isr21, 0x08, 0x8E);
-  // idt_set_gate(22, (unsigned long)_isr22, 0x08, 0x8E);
-  // idt_set_gate(23, (unsigned long)_isr23, 0x08, 0x8E);
-  // idt_set_gate(24, (unsigned long)_isr24, 0x08, 0x8E);
-  // idt_set_gate(25, (unsigned long)_isr25, 0x08, 0x8E);
-  // idt_set_gate(26, (unsigned long)_isr26, 0x08, 0x8E);
-  // idt_set_gate(27, (unsigned long)_isr27, 0x08, 0x8E);
-  // idt_set_gate(28, (unsigned long)_isr28, 0x08, 0x8E);
-  // idt_set_gate(29, (unsigned long long)_isr29, 0x08, 0x8E);
-  // idt_set_gate(30, (unsigned long)_isr30, 0x08, 0x8E);
-  // idt_set_gate(31, (unsigned long)_isr31, 0x08, 0x8E);
-  // idt_set_gate(0x80, (unsigned long long)_syscall, 0x08, 0x8E);
+  idt_set_gate(0, (unsigned long)_isr_generic, 0x08, 0x8E);
 }
--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -14,16 +14,19 @@
 
 extern "C" int main() {
   idt_install();
-  // isr_install();
+  isr_install();
+  // asm volatile("sti");
   // char* foo = (char*)0x10000;
   // register int* foo asm("ax");
   char string[] = "12345";
   write_base_int((unsigned long)string, 16, 0xA, 0x1, 32, 10);
+  // write_string(string, 0x0, 0xC, 79, 10);
 
   write_char('B', 0xA, 0x1, 30, 2);
   write_char('Q', 0xA, 0x1, 32, 4);
   write_char('D', 0xA, 0x1, 30, 4);
   write_base_int(0xb1A2E, 16, 0xA, 0x1, 32, 3);
   write_string("hello!", 0x0, 0xC, 79, 10);
+  int whatever = 1 / 0;
   return 0;
 }
